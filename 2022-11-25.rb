e01b5da27352d3a9b80e06673e9bf54b5bfe2cca
Samuel Path <samuel.path@shopify.com>
2022-11-28 11:59:15 +0100

    implement more efficient approach

--- 2022-11-25.rb
+++ 2022-11-25.rb
@@ -1,10 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/assert')
+require 'set'
 
 # Level: Easy
 # This problem was recently asked by Google.
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 # Bonus: Can you do this in one pass?
+
+######### SOLUTIONS #########
 
 # My initial naïve approach
 def any_two_numbers_add_up(list, k)
@@ -31,6 +34,19 @@ def amazing_one_liner_more_efficient(list, k)
   list.combination(2).any? { |a| a.sum == k }
 end
 
+# The most time efficient approach (linear time 0(n))
+# Manu you rock!
+def efficient_approach(list, k)
+  set_k_minus_n = Set.new(list.map { |n| k - n })
+  list.each do |n|
+    if (k / 2 != n && set_k_minus_n.include?(n))
+      return true
+    end
+  end
+  false
+end
+
+######### ASSERTIONS #########
 
 assert { any_two_numbers_add_up([10, 15, 3, 7], 17) == true }
 assert { any_two_numbers_add_up([10, 15, 3, 7], 27) == false }
@@ -52,3 +68,10 @@ assert { amazing_one_liner_more_efficient([10, 15, 3, 8], 18) == true }
 assert { amazing_one_liner_more_efficient([10, 15, 3, 8], 28) == false }
 assert { amazing_one_liner_more_efficient([1, 2], 3) == true }
 assert { amazing_one_liner_more_efficient([1, 2], 4) == false }
+
+assert { efficient_approach([10, 15, 3, 7], 17) == true }
+assert { efficient_approach([10, 15, 3, 7], 27) == false }
+assert { efficient_approach([10, 15, 3, 8], 18) == true }
+assert { efficient_approach([10, 15, 3, 8], 28) == false }
+assert { efficient_approach([1, 2], 3) == true }
+assert { efficient_approach([1, 2], 4) == false }
