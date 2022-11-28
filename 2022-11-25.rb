e07763228220cd26782ee29509f1e88f6fc6e834
Samuel Path <samuel.path@shopify.com>
2022-11-28 10:57:30 +0100

    add context

--- 2022-11-25.rb
+++ 2022-11-25.rb
@@ -1,5 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/assert')
 
+# Level: Easy
+# This problem was recently asked by Google.
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 # Bonus: Can you do this in one pass?
